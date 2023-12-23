68801a48d1f682bca8a4c3af29b687dfdb7f3e43
Mirko <nanetto_97@live.it>
2023-12-23 14:34:46 +0100

    punto di partenza

---
+++ src/app/service/dimensioneSchermo.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { DimensioneSchermoService } from './dimensioneSchermo.service';
+
+describe('Service: DimensioneSchermo', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DimensioneSchermoService]
+    });
+  });
+
+  it('should ...', inject([DimensioneSchermoService], (service: DimensioneSchermoService) => {
+    expect(service).toBeTruthy();
+  }));
+});
